bd86a64fe0a3ef52ef5a59d3c0ff2126790a76a1
richardhoff88 <86632564+richardhoff88@users.noreply.github.com>
2026-01-01 04:16:28 +0000

    Deploying to gh-pages from @ richardhoff88/richardhoff88.github.io@4f63678869e770a76e9d245a2241c4c0ae7e2ed1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,7 +16,25 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "post-pontryagin-s-maximum-principle-intuitively",
+        
+          title: "Pontryagin’s Maximum Principle, Intuitively",
+        
+        description: "An intuitive walkthrough of optimal control and Pontryagin’s Maximum Principle",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/pontryagin_blog/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
